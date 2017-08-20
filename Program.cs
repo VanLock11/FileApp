0facda4c0e79abd31f3f5d51433e2585191cc941
VanLock11 <van11khachatryan@gmail.com>
2017-08-20 13:28:14 +0400

    FileApp
    Changes file format to lip

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FilleApp
+{
+    class Program
+    {
+        //We check whether there is already a classified element with this name
+        public static bool ReadFile(string filePhat, string folder)
+        {
+            return File.Exists(folder + "\\" + Path.GetFileNameWithoutExtension(filePhat) + ".lip");
+        }
+        static void Main(string[] args)
+        {
+            string uzerName = Environment.UserName;
+            Console.WriteLine("\n\tHi " + uzerName + "!!\n");
+            //Path to the folder with the files
+            string folderPath = Environment.ExpandEnvironmentVariables("%SystemDrive%\\Users\\" + uzerName + "\\Desktop\\YourFile");
+            Console.WriteLine("Your secret-folder path : " + folderPath + "\n\n");
+            DirectoryInfo FolderCreat = new DirectoryInfo(folderPath);
+
+            //Check the existence of the folder
+            if (!FolderCreat.Exists)
+            {
+                FolderCreat.Create();
+                //Instruction
+                Console.WriteLine("Move your files to this folder and the program will make their secret copy .");
+            }
+            else
+            {
+                //An array with names and path files
+                string[] FileName = Directory.GetFiles(folderPath);
+                //Check for array elements
+                if (FileName.GetLength(0) != 0)
+                {
+
+                    foreach (string t in FileName)
+                    {
+                        //Check file privacy
+                        if (Path.GetExtension(t) != ".lip")
+                        {
+                            //Check for a secret copy
+                            if (!ReadFile(t, folderPath))
+                            {
+                                File.Copy(t, folderPath + "\\" + Path.GetFileNameWithoutExtension(t) + ".lip");
+                                Console.WriteLine(Path.GetFileNameWithoutExtension(t));
+                            }
+                            else
+                            {
+                                Console.WriteLine("You have secret copy " + Path.GetFileNameWithoutExtension(t) + " file!");
+                            }
+                        }
+                    }
+                    Console.WriteLine("\n\tHappy End !");
+                }
+                else
+                {
+                    Console.WriteLine("I`m sory ,but You ende file.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("FilleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FilleApp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("15ce285f-5ad6-4720-8539-3982bf68e887")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
